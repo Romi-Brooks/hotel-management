bc55ee5ac4a4a3f81a73b788a4d94b6d78044f09
RomiBrooks <qq1694821929@gmail.com>
2025-11-30 22:34:19 +0800

    upbuild

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, 'src') // 配置@为src路径
+            '@': path.resolve(__dirname, 'src'), // 配置@为src路径
+            base: process.env.NODE_ENV === 'production' ? '/https://github.com/Romi-Brooks/hotel-management/' : '/' // 仓库名
         }
     }
 })
